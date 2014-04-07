c2f6394fd5ac3ff21ee4f657e1bf1e54b3e8eabe
mfalce <falce.matthieu@gmail.com>
2014-04-07 17:10:30 +0200

    delete main in track_a_parcel.py

--- track_a_parcel/track_a_parcel.py
+++ track_a_parcel/track_a_parcel.py
@@ -118,23 +118,3 @@ class Tracker():
 
 def get_md5(_object):
     return hashlib.md5(_object.__str__().encode("utf8")).hexdigest()
-
-
-if __name__ == '__main__':
-    track_number = ["RC476805741CN", "RC477597197CN",
-                    "RC477408154CN", "RC476805741CN"]
-    tracker = Tracker()
-    ans = tracker.track(track_number)
-    #ans = get_track_info(track_number)
-    #ans = interresting_field(ans)
-
-    parcels = ans["parcels"]
-    last_event_tracking = [(p['events'][-1], p["trackingNumber"])
-                           for p in parcels]
-
-    for event, track_nb in last_event_tracking:
-        print(track_nb, ' : ')
-        print('   ' + event['date'], event['event'], event['location'],
-              sep=", ")
-
-    print(get_md5(ans))
