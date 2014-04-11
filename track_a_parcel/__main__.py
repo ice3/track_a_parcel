603e3e4ff324e532ccb3f598f26a537129a01b04
mfalce <falce.matthieu@gmail.com>
2014-04-11 19:29:22 +0200

    refactoring with creation of a main function

--- track_a_parcel/__main__.py
+++ track_a_parcel/__main__.py
@@ -19,10 +19,27 @@ def view(parcels, info_track):
         print()
 
 
-if __name__ == '__main__':
-    info_track = load_parcels("./data/mat.txt")
+def track(info_track):
     tracker = Tracker()
     ans = tracker.track(list(info_track))
     parcels = ans["parcels"]
-    view(parcels)
-    print(get_md5(ans))
+    return parcels
+
+
+def main(user):
+    print("Tracking parcels for {}".format(user))
+    user_parcelle = "./data/{}/parcelle.txt".format(user)
+    if not os.path.isfile(user_parcelle):
+        mess = "User {} does not have any parcels to track".format(user)
+        print(mess)
+    else:
+        info_track = load_parcels(user_parcelle)
+        parcels = track(info_track)
+        print(parcels)
+        view(parcels, info_track)
+
+
+if __name__ == '__main__':
+    args = create_args()
+    users = args.user
+    [main(user) for user in users]
