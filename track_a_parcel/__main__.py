8937685697378ca36ea7342d7dc5ef32ee79089f
mfalce <falce.matthieu@gmail.com>
2014-04-11 15:42:36 +0200

    put the view in a view function

--- track_a_parcel/__main__.py
+++ track_a_parcel/__main__.py
@@ -3,20 +3,23 @@
 from __future__ import print_function
 from track_a_parcel import Tracker, get_md5, load_parcels
 
-
-if __name__ == '__main__':
-    track_number = ["RC476805741CN", "RC477597197CN",
-                    "RC477408154CN", "RC476805741CN"]
-    tracker = Tracker()
-    ans = tracker.track(track_number)
-
-    parcels = ans["parcels"]
+def view(parcels):
     last_event_tracking = [(p['events'][-1], p["trackingNumber"])
                            for p in parcels]
 
     for event, track_nb in last_event_tracking:
-        print(track_nb, ' : ')
+        description, command, seller = info_track[track_nb]
+        print(description, '({}, {})'.format(seller, command), sep=' ')
         print(' -> ' + event['date'], event['event'], event['location'],
               sep=", ")
 
+
+if __name__ == '__main__':
+    track_number = ["RC476805741CN", "RC477597197CN",
+                    "RC477408154CN", "RC476805741CN"]
+    info_track = load_parcels("./data/mat.txt")
+    tracker = Tracker()
+    ans = tracker.track(track_number)
+    parcels = ans["parcels"]
+    view(parcels)
     print(get_md5(ans))
