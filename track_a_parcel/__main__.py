e7a7e0564dc16ab3be1b66ea752571e27c95f4dc
mfalce <falce.matthieu@gmail.com>
2014-04-11 19:25:59 +0200

    changed view

--- track_a_parcel/__main__.py
+++ track_a_parcel/__main__.py
@@ -3,13 +3,13 @@
 from __future__ import print_function
 from track_a_parcel import Tracker, get_md5, load_parcels
 
-def view(parcels):
     last_event_tracking = [(p['events'][-1], p["trackingNumber"])
                            for p in parcels]
 
     for event, track_nb in last_event_tracking:
         description, command, seller = info_track[track_nb]
-        print(description, '({}, {})'.format(seller, command), sep=' ')
+        print('[{}]'.format(track_nb), description,
+              '({}, {})'.format(seller, command), sep=' ')
         print('  -> ' + event['date'], event['event'], event['location'],
               sep=", ")
         print()
