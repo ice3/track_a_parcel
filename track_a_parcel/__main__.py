b5b4bd387f0ad56bd79c8ca0b29ac7ddfb7b9786
mfalce <falce.matthieu@gmail.com>
2014-04-14 12:29:01 +0200

    lots of things  * easy testing view (need refactor to be added to tests)  * custom number of events to view

--- track_a_parcel/__main__.py
+++ track_a_parcel/__main__.py
@@ -6,16 +6,22 @@ import os
 from track_a_parcel import Tracker, get_md5, load_parcels
 from command_line import create_args
 
-def view_info(parcels, info_track):
-    last_event_tracking = [(p['events'][-1], p["trackingNumber"])
-                           for p in parcels]
 
-    print(last_events_tracking)
-    for events, track_nb in last_events_tracking:
+def last_n_events(parcels, n):
+    nb_events = -n  # we want the N last events
+    # get the last events
+    tmp = [(p['events'][nb_events:], p["trackingNumber"]) for p in parcels]
+    # reverse : last events first
+    last_events_tracking = [(e[::-1], trNb) for e, trNb in tmp]
+    return last_events_tracking
+
+
+def view_info(parcels, info_track, nb_events):
+    for events, track_nb in last_n_events(parcels, nb_events):
         description, command, seller = info_track[track_nb]
         print('[{}]'.format(track_nb), description,
               '({}, {})'.format(seller, command), sep=' ')
-        for event in events :
+        for event in events:
             print('  -> ' + event['date'], event['event'], event['location'],
                   sep=", ")
         print()
@@ -23,7 +29,7 @@ def view_info(parcels, info_track):
 
 def view_loading(info_track):
     print('Found {} parcels :'.format(len(info_track)))
-    print(*['  * ' + i for i in list(info_track)], sep = ' \n')
+    print(*['  * ' + i for i in list(info_track)], sep=' \n')
 
 
 def track(info_track):
@@ -33,7 +39,7 @@ def track(info_track):
     return parcels
 
 
-def main(user):
+def main(user, nb_events):
     print("Tracking parcels for {}".format(user))
     user_parcelle = "./data/{}/parcelle.txt".format(user)
     if not os.path.isfile(user_parcelle):
@@ -43,10 +49,18 @@ def main(user):
         info_track = load_parcels(user_parcelle)
         view_loading(info_track)
         parcels = track(info_track)
-        view_info(parcels, info_track, 2)
+        view_info(parcels, info_track, nb_events)
 
 
 if __name__ == '__main__':
     args = create_args()
-    users = args.user
-    [main(user) for user in users]
+
+    if args.test_view:
+        from view2_data import data, info_track
+        view_info(data, info_track, args.nb_events)
+        import sys
+        sys.exit(1)
+
+    [main(user, args.nb_events) for user in args.users]
+
+
